de445a573e9a889a7c405d7ce25488248b0e8de4
bklynate <nathanielcfa@gmail.com>
2016-09-28 18:43:56 -0400

    Add gulp tasks and css workflow pipeline

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var addVariablesToCss = require('postcss-simple-vars');
+var nestedCssStructures = require('postcss-nested');
+
+gulp.task('default', function(){
+  console.log("hooray, you created a gulp task");
+});
+
+gulp.task('html', function(){
+  console.log("Imagine some useful HTML magic happening here!");
+});
+
+gulp.task('styles', function(){
+  return gulp.src('./app/assets/styles/styles.css')
+    .pipe(postcss([addVariablesToCss,nestedCssStructures,autoprefixer]))
+    .pipe(gulp.dest('./app/temp/styles'));
+});
+
+gulp.task('watch', function(){
+
+  watch('./app/index.html', function(){
+    gulp.start('html');
+  })
+
+  watch('./app/assets/styles/**/*.css', function(){
+    gulp.start('styles');
+  })
+})
