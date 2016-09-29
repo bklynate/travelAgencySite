079f9819ad226630ca91b2955fd5fad3cb8cd9db
bklynate <nathanielcfa@gmail.com>
2016-09-29 18:38:19 -0400

    Add more gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var addVariablesToCss = require('postcss-simple-vars');
 var nestedCssStructures = require('postcss-nested');
+var cssImport = require('postcss-import');
 
 gulp.task('default', function(){
   console.log("hooray, you created a gulp task");
@@ -15,7 +16,7 @@ gulp.task('html', function(){
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([addVariablesToCss,nestedCssStructures,autoprefixer]))
+    .pipe(postcss([cssImport,addVariablesToCss,nestedCssStructures,autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
