5d7c8837959c346646ebf0637d22b9da5b2cddc3
bklynate <nathanielcfa@gmail.com>
2016-09-30 21:50:06 -0400

    Edit variable name

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var watch = require('gulp-watch');
 var postcss = require('gulp-postcss');
 var autoprefixer = require('autoprefixer');
 var addVariablesToCss = require('postcss-simple-vars');
-var nestedCssStructures = require('postcss-nested');
+var addNestedCssStructures = require('postcss-nested');
 var cssImport = require('postcss-import');
 var browserSync = require('browser-sync').create();
 
@@ -11,13 +11,13 @@ gulp.task('default', function(){
   console.log("hooray, you created a gulp task");
 });
 
-gulp.task('html', function(){
-  console.log("Imagine some useful HTML magic happening here!");
-});
+// gulp.task('html', function(){
+//   console.log("Imagine some useful HTML magic happening here!");
+// });
 
 gulp.task('styles', function(){
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssImport,addVariablesToCss,nestedCssStructures,autoprefixer]))
+    .pipe(postcss([cssImport,addVariablesToCss,addNestedCssStructures,autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 });
 
